5ba8cb49b211cb4f111d9c4e5d1b5ea92720ca2b
David Ringo <davidmringo@gmail.com>
2017-12-19 09:36:37 -0700

    CLEAN: put custom.el stuff in its own file

--- emacs/init.el
+++ emacs/init.el
@@ -196,7 +196,9 @@
          ("C-h w" . helm-descbinds)))
 
 (use-package which-key
-  :config (setq which-key-idle-delay 0.5))
+  :config
+  (setq which-key-idle-delay 0.5)
+  (which-key-mode))
 
 ;; Local "packages"
 (let ((theme-dir (expand-file-name "lisp/themes" "~/.emacs.d")))
@@ -215,7 +217,6 @@
 (use-package my-c-setup
   :ensure f
   :load-path "lisp/")
-
 
 ;; for Ackley's Living Computation course. Java-derived major-mode
 (use-package ulam-mode
@@ -242,7 +243,6 @@
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 (add-hook 'after-make-frame-functions #'client-fix-path)
-
 
 ;; Always move to help window after opening (easier to close)
 (setq help-window-select t)
@@ -288,7 +288,7 @@
 (setq ring-bell-function 'my/do-nothing)
 
 ;; Make the fringes smaller
-(setq default-left-fringe-width 5
+(setq default-left-fringe-width  5
       default-right-fringe-width 5)
 
 ;; prefer vertical splits
@@ -338,53 +338,10 @@
 (setq ps-multibyte-buffer :bdf-font-except-latin)
 (setq bdf-directory-list "/usr/share/emacs/fonts/bdf")
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   [("#181818" . "#282828")
-    ("#ab4642" . "#dc9656")
-    ("#a1b56c" . "#383838")
-    ("#f7ca88" . "#383838")
-    ("#7cafc2" . "#585858")
-    ("#ba8baf" . "#b8b8b8")
-    ("#86c1b9" . "#d8d8d8")
-    ("#ffffff" . "#ffffff")])
- '(cursor-type (quote box))
- '(custom-safe-themes
-   (quote
-    ("8d55185d74000bc7caaea2105f4b8410cd0f82e1148e7ba93e2bcdcea275dbbf" "3c9f63378e7f1d64452d063b3475c8c0e7d8726b0c681c1e3943b2c605a26bed" "6fa6524f51574b984e239e203b59b2f81da1a1b282c2ac09bba8e6198892c924" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "78559045fb299f3542c232166ad635c59cf0c6578d80a58b885deafe98a36c66" "88b3e618978518e7117518706043cd68b55eaab0059e6e0528cf876f4ca0acd6" default)))
- '(package-selected-packages
-   (quote
-    (treemacs which-key keyfreq pandoc-mode mmm-mode dokuwiki-mode alect-themes basic-theme helm-tramp my-utils edit-indirect math-symbol-lists helm-idris idris-mode whitespace-cleanup-mode racket-mode helm-unicode haskell-snippets yasnippet pandoc exec-path-from-shell beacon f company-ghc rainbow-mode kurecolor fill-column-indicator nav-flash pdf-tools centered-window-mode gitignore-mode undo-tree rainbow-delimiters smartparens use-package)))
- '(safe-local-variable-values (quote ((intero-targets "cs558:lib" "cs558:test:spec"))))
- '(vc-annotate-background "#ffffff")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#ab4642")
-     (50 . "#dc9656")
-     (80 . "#f7ca88")
-     (110 . "#a1b56c")
-     (140 . "#86c1b9")
-     (170 . "#7cafc2")
-     (200 . "#ba8baf")
-     (230 . "#a16046")
-     (260 . "#181818")
-     (290 . "#282828")
-     (320 . "#383838")
-     (350 . "#585858"))))
- '(vc-annotate-very-old-color "#585858")
- '(yas-global-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Everson Mono"))))
- '(fringe ((t (:background "#1b1b1b")))))
+;; Keep Custom elsewhere
+(defconst my-custom-file (expand-file-name "lisp/my-custom.el" "~/.emacs.d"))
+(setq custom-file my-custom-file)
+(load custom-file)
 
 ;; let Custom declare this safe before loading it
 (load-theme 'greymatters-dark)
---
+++ emacs/lisp/my-custom.el
@@ -0,0 +1,53 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   [("#181818" . "#282828")
+    ("#ab4642" . "#dc9656")
+    ("#a1b56c" . "#383838")
+    ("#f7ca88" . "#383838")
+    ("#7cafc2" . "#585858")
+    ("#ba8baf" . "#b8b8b8")
+    ("#86c1b9" . "#d8d8d8")
+    ("#ffffff" . "#ffffff")])
+ '(cursor-type (quote box))
+ '(custom-safe-themes
+   (quote
+    ("8d55185d74000bc7caaea2105f4b8410cd0f82e1148e7ba93e2bcdcea275dbbf"
+     "3c9f63378e7f1d64452d063b3475c8c0e7d8726b0c681c1e3943b2c605a26bed"
+     "6fa6524f51574b984e239e203b59b2f81da1a1b282c2ac09bba8e6198892c924"
+     "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a"
+     "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6"
+     "78559045fb299f3542c232166ad635c59cf0c6578d80a58b885deafe98a36c66"
+     "88b3e618978518e7117518706043cd68b55eaab0059e6e0528cf876f4ca0acd6"
+     default)))
+ '(package-selected-packages
+   (quote
+    (treemacs which-key keyfreq pandoc-mode mmm-mode dokuwiki-mode alect-themes basic-theme helm-tramp my-utils edit-indirect math-symbol-lists helm-idris idris-mode whitespace-cleanup-mode racket-mode helm-unicode haskell-snippets yasnippet pandoc exec-path-from-shell beacon f company-ghc rainbow-mode kurecolor fill-column-indicator nav-flash pdf-tools centered-window-mode gitignore-mode undo-tree rainbow-delimiters smartparens use-package)))
+ '(safe-local-variable-values (quote ((intero-targets "cs558:lib" "cs558:test:spec"))))
+ '(vc-annotate-background "#ffffff")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ab4642")
+     (50 . "#dc9656")
+     (80 . "#f7ca88")
+     (110 . "#a1b56c")
+     (140 . "#86c1b9")
+     (170 . "#7cafc2")
+     (200 . "#ba8baf")
+     (230 . "#a16046")
+     (260 . "#181818")
+     (290 . "#282828")
+     (320 . "#383838")
+     (350 . "#585858"))))
+ '(vc-annotate-very-old-color "#585858")
+ '(yas-global-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:family "Everson Mono"))))
+ '(fringe ((t (:background "#1b1b1b")))))
