2c0c8cb9b963a065d9a7e3ac2e8c00752f4275ad
David Ringo <davidmringo@gmail.com>
2018-01-12 14:20:58 -0700

    ADD: update my-custom

--- emacs/lisp/my-custom.el
+++ emacs/lisp/my-custom.el
@@ -15,10 +15,10 @@
  '(cursor-type (quote box))
  '(custom-safe-themes
    (quote
-    ("8d55185d74000bc7caaea2105f4b8410cd0f82e1148e7ba93e2bcdcea275dbbf" "3c9f63378e7f1d64452d063b3475c8c0e7d8726b0c681c1e3943b2c605a26bed" "6fa6524f51574b984e239e203b59b2f81da1a1b282c2ac09bba8e6198892c924" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "78559045fb299f3542c232166ad635c59cf0c6578d80a58b885deafe98a36c66" "88b3e618978518e7117518706043cd68b55eaab0059e6e0528cf876f4ca0acd6" default)))
+    ("04317336f6010f89e444b5ffc5b5a273290cd4b24a21bc13a5785179d7998a26" "8d55185d74000bc7caaea2105f4b8410cd0f82e1148e7ba93e2bcdcea275dbbf" "3c9f63378e7f1d64452d063b3475c8c0e7d8726b0c681c1e3943b2c605a26bed" "6fa6524f51574b984e239e203b59b2f81da1a1b282c2ac09bba8e6198892c924" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "78559045fb299f3542c232166ad635c59cf0c6578d80a58b885deafe98a36c66" "88b3e618978518e7117518706043cd68b55eaab0059e6e0528cf876f4ca0acd6" default)))
  '(package-selected-packages
    (quote
-    (flycheck-rtags company-quickhelp flycheck-irony company-irony irony-eldoc irony rtags cmake-ide cmake-mode company-c-headers helm-gtags treemacs which-key keyfreq pandoc-mode mmm-mode dokuwiki-mode alect-themes basic-theme helm-tramp my-utils edit-indirect math-symbol-lists helm-idris idris-mode whitespace-cleanup-mode racket-mode helm-unicode haskell-snippets yasnippet pandoc exec-path-from-shell beacon f company-ghc rainbow-mode kurecolor fill-column-indicator nav-flash pdf-tools centered-window-mode gitignore-mode undo-tree rainbow-delimiters smartparens use-package)))
+    (atomic-chrome counsel-etags helm-projectile projectile-ripgrep projectile flycheck-rtags company-quickhelp flycheck-irony company-irony irony-eldoc irony rtags cmake-ide cmake-mode company-c-headers helm-gtags treemacs which-key keyfreq pandoc-mode mmm-mode dokuwiki-mode alect-themes basic-theme helm-tramp my-utils edit-indirect math-symbol-lists helm-idris idris-mode whitespace-cleanup-mode racket-mode helm-unicode haskell-snippets yasnippet pandoc exec-path-from-shell beacon f company-ghc rainbow-mode kurecolor fill-column-indicator nav-flash pdf-tools centered-window-mode gitignore-mode undo-tree rainbow-delimiters smartparens use-package)))
  '(safe-local-variable-values
    (quote
     ((flycheck-clang-language-standard "c++11")
