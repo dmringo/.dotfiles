484190c05135f455b3edb9156b7a5f252a240a8f
David <davidmringo@gmail.com>
2018-11-19 11:03:15 -0700

    misc emacs config updates
    - Add an org-mode block template for clocktables
    
      org-mode's time-tracking facilities are still a little awkward but the
      clocktable is a nice feature
    
    - Prefer `smart-parens` suggested bindings rather than manually setting bindings
    
      See `sp-base-key-bindings` docs for info + alternatives.
    
    - Use `clang-format` package for c/c++ prettification
    
    - Use `racket-mode` for Racket
    
    - Use `lsp-go` in conjunction with `go-mode`
    
      This requires the sourcegraph Go language server to be installed
    
    - As always, custom.el updates

--- emacs/init.el
+++ emacs/init.el
@@ -53,7 +53,9 @@
           ;; latexmk is a little more consistent than pdflatex
           org-latex-pdf-process (list "latexmk -f -pdf %f"))
     ;; make #+NAME easy-template
-    (add-to-list 'org-structure-template-alist '("n" "#+NAME: ?"))))
+    (add-to-list 'org-structure-template-alist '("n" "#+NAME: ?"))
+    (add-to-list 'org-structure-template-alist
+                 '("ct" "#+BEGIN: clocktable ?\n#+END:"))))
 
 (use-package ox-twbs)
 (use-package ox-gfm)
@@ -107,6 +109,7 @@
 
 (use-package smart-mode-line)
 (use-package smartparens
+  :init (setq sp-base-key-bindings 'sp)
   :config (progn
 	    (add-hook 'prog-mode-hook 'smartparens-mode)
 	    (sp-with-modes 'markdown-mode
@@ -115,12 +118,7 @@
 			   (sp-local-pair "_" "_")
 			   (sp-local-pair "$" "$"))
 	    (sp-with-modes 'c++-mode
-			   (sp-local-pair "/*" "*/")))
-  :bind (:map smartparens-mode-map
-              ("C-c u w" . sp-unwrap-sexp)
-              ("C-M-f"   . sp-forward-sexp)
-              ("C-M-b"   . sp-backward-sexp)
-              ("C-M-k"   . sp-kill-sexp)))
+			  (sp-local-pair "/*" "*/"))))
 
 ;; Whitespace-related
 (use-package whitespace-cleanup-mode
@@ -167,8 +165,6 @@
   :config
   (setq cquery-executable (expand-file-name "cquery" "~/.local/bin/")))
 
-(use-package ccls)
-
 (defun my/maybe-enable-c++-lsp-server ()
   (interactive)
   (when (locate-dominating-file default-directory "compile_commands.json")
@@ -177,6 +173,8 @@
           (message "enabling c++ lsp server")
           (lsp-ccls-enable))
       (user-error nil))))
+
+(use-package clang-format)
 
 (add-hook 'c++-mode-hook 'company-mode)
 (add-hook 'c++-mode-hook #'my/maybe-enable-c++-lsp-server)
@@ -299,6 +297,8 @@
 
 (use-package direnv)
 
+;; Racket
+(use-package racket-mode)
 
 ;; Python stuff
 (use-package py-autopep8)
@@ -357,6 +357,10 @@ This does two things:
 
 (use-package elm-mode)
 (use-package go-mode)
+(use-package lsp-go
+  :init
+  (add-hook 'go-mode-hook #'lsp-go-enable))
+
 (use-package treemacs
   :config
   (treemacs-git-mode 'extended)
