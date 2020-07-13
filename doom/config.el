97d077447ddc5933a2518ac3d9fcd59f4211b333
David Ringo <davidmringo@gmail.com>
2020-07-13 18:15:56 -0400

    [doom] add some cc-mode prefs

--- doom/config.el
+++ doom/config.el
@@ -94,6 +94,13 @@
   (setq! projectile-switch-project-action 'projectile-vc))
 
 
+(setq! c-basic-offset 2)
+
+(after! cc-mode
+  (fset 'c-indent-region 'clang-format-region)
+  (fset 'c-indent-line-or-region 'clang-format))
+
+
 (setq! haskell-process-type 'stack-ghci)
 
 
@@ -101,7 +108,7 @@
 (advice-add
  'shell-command
  :after
- (defun shell-rename-buffer (cmd &optional o-buf e-buf)
+ (defun shell-rename-buffer (cmd &optional o-buf _)
    "Preserve shell-command output in a well-named buffer unless
 one is manually specified."
    (unless o-buf
@@ -112,3 +119,21 @@ one is manually specified."
              (if async? "*Async Shell Command*" "*Shell Command Output*")))
        (with-current-buffer orig-bufname
          (rename-buffer new-bufname t))))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval local-set-key
+           (kbd "C-c C-k")
+           (lambda nil
+             (interactive)
+             (compile "make -k -j 8"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
